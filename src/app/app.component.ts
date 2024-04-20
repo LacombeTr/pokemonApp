fd5da1ea5d680c83f8e9cd693b3afae27f1069e9
Tristan Lacombe <tlacombevulca@outlook.fr>
2024-04-20 20:27:49 +0200

    avancé dans le tutoriel jusqu'a 2h07, ajouté un template separé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { POKEMONS } from './mock-pokemons'; // on importe la variable 'POKEMONS' depuis Mock-Pokemon (c'est un tableau)
 import { Pokemon } from './pokemon'; // on importe le type (d'objet) 'pokemon' depuis 
+import { find } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Liste de pokemons</h1>`,
+  templateUrl: 'app.component.html',
 })
 
 export class AppComponent implements OnInit{
 
-  pokemonList: Pokemon[] = POKEMONS; //on dit que la variable 'pokemonList' est égale a la variable importée 'POKEMONS'
+  pokemonList: Pokemon[] = POKEMONS; //on dit que la propriété 'pokemonList' et de type 'Pokemon' est égale a la variable importée 'POKEMONS'
                                      //et que elle ne peut être que de type 'Pokemon'
+  pokemonSelected: Pokemon;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[5]) // on passe a la methode le 5eme objet du tableau 'pokemonList'
   }
 
-  selectPokemon(pokemon: Pokemon){                      // J'indique que l'objet passé a la méthode est de type 'Pokemon'
-    console.log(`Vous avez cliqué sur ${pokemon.name}`); // on va chercher l'attribut 'name' de l'objet 'pokemon'
+  selectPokemon(pokemonID: string){                      // J'indique que l'objet passé a la méthode est de type 'Pokemon'
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonID)
+
+    if(pokemon){ //Si l'objet pokemon existe (a l'id specifié)
+        console.log(`Vous avez cliqué sur ${pokemon.name}`); // on va chercher l'attribut 'name' de l'objet 'pokemon'
+        this.pokemonSelected = pokemon
+      }
+    else{
+      console.error(`Il n'y a pas de pokemon avec cet ID`);
+    }
   }
 }
