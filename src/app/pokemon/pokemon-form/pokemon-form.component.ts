eb3e604302a164cdb785877f5d77b04dbf6041f5
Tristan Lacombe <tlacombevulca@outlook.fr>
2024-04-21 18:52:57 +0200

    Ajouté le component Form et ses methodes dans son Ts

---
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pokemon-form',
+  templateUrl: './pokemon-form.component.html',
+  styles: ``
+})
+
+export class PokemonFormComponent implements OnInit{
+  @Input() pokemon: Pokemon; // quand 'app-pokemon-form' est appellé il recquiert une propriété d'entrée de type 'Pokemon'
+  typeList: string[];
+
+  constructor(
+    private router: Router,
+    private pokemonservice:PokemonService
+  ){}
+
+  ngOnInit(){
+    this.typeList = this.pokemonservice.getPokemonTypeList();// importer la liste des types de pokemon
+  }
+
+  hasType(type: string): boolean { //permet de precocher les types qu'a le pokemon
+    return this.pokemon.types.includes(type);
+  }
+
+  selectType($event: Event, type: string){ // permet de selectionner de nouveaux types pour le pokemon, reagi a un event
+    const isChecked: boolean = ($event.target as HTMLInputElement).checked; // indique si l'utilisateur a coché ou non la checkbox
+
+    if(isChecked){
+      this.pokemon.types.push(type); //Si un type est coché alors on ajoute (on push) le type coché dans les type du pokemon
+    } else{
+      const indexType = this.pokemon.types.indexOf(type); // Je cherche l'index du type decoché
+      this.pokemon.types.splice(indexType, 1) // Je retire le type (grace a la fonction splice) situé à l'index 'IndexType'
+    }
+  }
+
+  onSubmit(){ // a l'envoi du formulaire
+    console.log('Submitted')
+    this.router.navigate(['/pokemon', this.pokemon.id])
+  }
+}
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { Pokemon } from './pokemon';
@@ -8,6 +9,7 @@ import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonService } from './pokemon.service';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 
 const pokemonRoutes: Routes = [ //declarer les routes les plus specifique en premier car c'est lu du haut vers le bas
   { path: 'pokemon', component: ListPokemonComponent}, //liste pokemon
@@ -19,10 +21,12 @@ const pokemonRoutes: Routes = [ //declarer les routes les plus specifique en pre
     ListPokemonComponent,
     DetailPokemonComponent,
     BorderCardDirective,
-    PokemonTypeColorPipe, //on l'utilise aue ici dans le cadre des pokemons
+    PokemonTypeColorPipe,
+    PokemonFormComponent, //on l'utilise aue ici dans le cadre des pokemons
   ],
   imports: [
     CommonModule, // module qui conteint des logiaues telles que ngIf et ngFor
+    FormsModule,
     RouterModule.forChild(pokemonRoutes)
   ],
   providers: [PokemonService] // Indique les services qui fournissent ce module
